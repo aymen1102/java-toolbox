234af349c9202ebe6033cf11a228656a0a1cc645
aymen1102 <baroudi.aymen@gmail.com>
2022-02-21 14:24:04 +0100

    learning examples

--- src/main/java/java8/stream/model/Product.java
+++ src/main/java/java8/stream/model/Product.java
@@ -1,33 +1,41 @@
-package java8.stream;
+package java8.stream.model;
 
-public class Product{
+public class Product {
 
-    int id;
-    String name;
-    float price;
+    public int id;
+    public String name;
+    public float price;
+
     public int getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public float getPrice() {
         return price;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setPrice(float price) {
         this.price = price;
     }
+
     public Product(int id, String name, float price) {
         this.id = id;
         this.name = name;
         this.price = price;
     }
+
     @Override
     public String toString() {
         return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
