14f29e91213516f838f351d34e50434042489492
Aymen BAROUDI <baroudi.aymen@gmail.com>
2022-08-04 22:42:08 +0200

    add new features to improve the project

--- src/main/java/java11/StringFeatures/StringFeaturesExamples.java
+++ src/main/java/java11/StringFeatures/StringFeaturesExamples.java
@@ -1,14 +1,17 @@
-package java11.StringFeatures;
+package java11.stringFeatures;
 
 public class StringFeaturesExamples {
 
     public static void main(String[] args) {
+        /** isBlank() */
         System.out.println(" ".isBlank()); //true
 
+        /** strip() */
         System.out.println(" Ay men ".strip().replace(" ","*")); // Ay*men
         System.out.println(" Ay men ".stripLeading().replace(" ","*")); // Ay*men*
         System.out.println(" Ay men ".stripTrailing().replace(" ","*")); // *Ay*men
 
+        /** lines() */
         "Line1\nLine2\nLine3".lines().forEach(System.out::println); //Line1 Line2 Line3
 
         System.out.println((String) "Aymen".transform(s -> s.substring(2))); // men
---
+++ src/main/java/java11/lambdaLocalVariable/LambdaLocalVariableExample.java
@@ -0,0 +1,14 @@
+package java11.lambdaLocalVariable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaLocalVariableExample {
+
+    public static void main(String[] args) {
+        List<String> programmingLanguages = Arrays.asList("Java","Javascript","Python");
+        programmingLanguages.stream()
+                .map((var x)-> x.toUpperCase())
+                .forEach(System.out::println);   //JAVA     JAVASCRIPT      PYTHON
+    }
+}
